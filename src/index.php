6c07a8c6ee871c253aa01bb1374c11a8ecebcb42
Yuriy <focus.us3r@gmail.com>
2023-12-17 13:18:45 +0100

    php loop syntax

--- src/index.php
+++ src/index.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First lesson</title>
-</head>
-<body>
-    <main>
-        <h1>Lorem, ipsum dolor.</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ad corrupti similique obcaecati natus laboriosam iure debitis adipisci dolorum impedit aliquid asperiores consectetur blanditiis expedita, iusto quae, perspiciatis sint. Provident pariatur perspiciatis quisquam!
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem aliquid debitis aliquam nesciunt quidem, dolor ad? Soluta provident qui esse!
-    </main>
-    <p style="color: blue; font-size: 16px;">Этот текст синего цвета и размера шрифта 16px.</p>
+<?php
 
-</body>
-</html>
+
+$c = 1;
+while ($c < 6) {
+    echo "Текущее значение счетчика while: $c<br>";
+    $c++;
+}
+
+echo "<hr>";
+
+for ($c = 1; $c < 6; $c++) {
+    echo "Текущее значение счетчика for: $c<br>";
+}
