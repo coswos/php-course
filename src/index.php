9f1b851540c8824b4a2c65cf4506cb62bfa4a6dc
Yuriy <focus.us3r@gmail.com>
2023-12-17 13:46:33 +0100

    iteration with for && while

--- src/index.php
+++ src/index.php
@@ -3,12 +3,28 @@
 
 $c = 1;
 while ($c < 6) {
-    echo "Текущее значение счетчика while: $c<br>";
+    echo "Counter while: $c<br>";
     $c++;
 }
 
 echo "<hr>";
 
 for ($c = 1; $c < 6; $c++) {
-    echo "Текущее значение счетчика for: $c<br>";
+    echo "Counter for: $c<br>";
+}
+
+echo "<hr>";
+
+$colors = ["red", "green", "blue", "yellow"];
+
+for ($i = 0; $i < count($colors); $i++) {
+    echo $colors[$i], PHP_EOL;;
+}
+
+echo "<hr>";
+
+$index = 0;
+while ($index < count($colors)) {
+    echo $colors[$index], PHP_EOL;
+    $index++;
 }
